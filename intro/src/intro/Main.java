872ba8f992f37bd7eff8a07daf60525f2e3ab5f7
kanukbaj <denizhan.arac@yahoo.com.tr>
2021-05-01 18:32:30 +0300

    Jva giriş - intro

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,55 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//camelCase
+		//Don't repeat yourself
+		String internetSubeButonu ="Ýnternet Þubeye gir";
+		double dolarDun =8.20;
+		double dolarBugun =8.20;
+		int vade =36;
+		boolean dustuMu= false;
+		
+		
+		System.out.println(internetSubeButonu);
+		
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar düþtü resmi");
+		}else if(dolarBugun>dolarDun){
+			System.out.println("Dolar yükseldi resmi");
+		}
+		
+		else {System.out.println("Dolar eþittir resmi");
+		
+		}
+		
+		String kredi1="Hýzlý kredi";
+		String kredi2="Mutlu emekli kredisi";
+		String kredi3="Konut kredisi";
+		String kredi4="Çiftci kredisi";
+		String kredi5="Msb kredisi";
+		
+		String[] krediler = 
+				{
+				"Hýzlý kredi",
+				"Mutlu emekli kredisi",
+				"Konut kredisi",
+				"Çiftci kredisi",
+				"Msb kredisi", 
+				"Meb kredisi",
+				"Kültür Bakanlýðý kredisi"
+				};
+		//foreach
+		for(String kredi : krediler) {
+			System.out.println(kredi);			
+		
+		}
+		for(int i=0; i<krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+
+	}
+
+}
