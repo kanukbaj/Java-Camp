7bdf6befc6a5ed58637ff7198251e348b1f1d1e7
kanukbaj <denizhan.arac@yahoo.com.tr>
2021-05-01 18:34:20 +0300

    oop intro

---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,29 @@
+package oopIntro;
+
+import java.util.Iterator;
+
+public class Main {	
+
+	public static void main(String[] args) {
+		
+		Product product1 = new Product(1, "Lenovo v14", 15000, "16 GB Ram", 10);//örnek oluþturma, referans oluþturma, instance oluþturma
+		
+				
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16 Gb Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
+		
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
+		
+		
+		
+		
+	
+		
+	}
+
+}
