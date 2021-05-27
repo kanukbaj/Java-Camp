02ddf73ba8fa1d5dca798c08d862135b940ed8a7
kanukbaj <denizhan.arac@yahoo.com.tr>
2021-05-27 17:55:49 +0300

    Ders-7 Odev-3

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
 @RestController
@@ -25,8 +28,17 @@ public class JobPositionsController {
 
 	@GetMapping("/getall")
 	public List<JobPosition> getAll(){
-		return this.jobPositionService.getAll();
-		
+		return this.jobPositionService.getAll();	
+	}
+	
+	@GetMapping("/getbyname")
+	public JobPosition getByPositionName(String positionName) {
+		return this.jobPositionService.getByPositionName(positionName);
+	}
+	
+	@PostMapping("add")
+	public Result addNew(@RequestBody JobPosition jobPosition) {
+		return this.jobPositionService.addNew(jobPosition);
 	}
 }
 
