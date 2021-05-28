8191675747d75d3551f3db5327195e3300bf7b0d
kanukbaj <denizhan.arac@yahoo.com.tr>
2021-05-28 16:30:19 +0300

    20.05.2021 düzeltme

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
@@ -25,6 +28,23 @@ public class JobPositionManager implements JobPositionService{
 	public List<JobPosition> getAll() {
 		
 		return this.jobPositionDao.findAll();
+	}
+
+	@Override
+	public JobPosition getByPositionName(String positionName) {
+			return this.jobPositionDao.findByPositionName(positionName);
+	}
+
+	@Override
+	public Result addNew(JobPosition jobPosition) {
+		if(jobPosition.getPositionName() == null || jobPosition.getPositionName() == "")
+		return new ErrorResult("İş pozisyon ismi boş bırakılamaz.");
+		
+		if(getByPositionName(jobPosition.getPositionName()) != null)
+			return new ErrorResult("Aynı isimde iki adet iş pozisyonu oluşturulamaz.");
+		
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("İş pozisyone başarıyla eklendi.");
 	}
 
 }
